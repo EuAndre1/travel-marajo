e26a954a6980b210b57013ba27d526d85cd4b6e1
EuAndre1 <141600591+EuAndre1@users.noreply.github.com>
2026-03-03 19:06:03 +0000

    fix: corrige pastas do prisma

---
+++ teste-db.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+  try {
+    console.log("🚀 Conectando ao banco no Railway...")
+    const viagens = await prisma.trip.findMany() 
+    
+    console.log("✅ Dados encontrados:")
+    console.table(viagens)
+  } catch (e) {
+    console.error("❌ Erro de conexão:", e)
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+main()
