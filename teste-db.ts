f4a522fa98cd326ac440d5d6454fa4b315137c4d
EuAndre1 <141600591+EuAndre1@users.noreply.github.com>
2026-03-04 04:14:38 +0000

    fix: env vars for vercel

--- teste-db.ts
+++ teste-db.ts
@@ -1,5 +1,16 @@
+import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
+
+const connectionString = process.env.DATABASE_URL
+
+const pool = new pg.Pool({ 
+  connectionString 
+})
+
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
 
 async function main() {
   try {
